71d09892985c95d2f38e8f27f980b0d9f3e4a51e
IleanaEscobar <izescobar5@gmail.com>
2026-02-04 00:26:14 -0800

    Update App.js

--- the-visionaries/src/App.js
+++ the-visionaries/src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+// later implement for webrtc
+// import { io } from "socket.io-client";
+
+// const socket = io("https://localhost:8443", { secure: true });
+
+// socket.on("connect", () => console.log("Connected to signaling server"));
+// socket.on("answer", (data) => console.log("Received answer:", data));
+
+
 function App() {
   return (
     <div className="App">
